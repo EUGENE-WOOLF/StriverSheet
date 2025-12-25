5b5978c8de3bb00a86761369da71fe531e89123d
Yuri <eugenewoolf220205@gmail.com>
2025-12-25 15:38:52 +0530

    vectors complete

--- stl/1.cpp
+++ stl/1.cpp
@@ -1,67 +1,93 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 void vectors();
 
-int main(){
-    //pair 
-    
-    std::pair<int , int> p = {1,3};
-    std::pair<int, std::pair<int, int>> pair2 = {1, {2, 3}};
-    std::pair<int, int> arr[] = {{1,2}, {2,3}};
-    
-    
-    // std::cout << p.first << " " << p.second << "\n";
-    // std::cout << pair2.first << " " << pair2.second.first << " " << pair2.second.second << "\n"; 
+int main() {
+    // ===== Pair Basics =====
+    pair<int, int> p = {1, 3};
+    pair<int, pair<int, int>> pair2 = {1, {2, 3}};
+    pair<int, int> arr[] = {{1, 2}, {2, 3}};
 
+    // cout << p.first << " " << p.second << "\n";
+    // cout << pair2.first << " "
+    //      << pair2.second.first << " "
+    //      << pair2.second.second << "\n";
 
     vectors();
-
     return 0;
 }
 
-void vectors(){
-    std::vector<int> v;
+void vectors() {
+
+    // ===== Basic Vector Operations =====
+    vector<int> v;
     v.push_back(1);
     v.emplace_back(2);
 
-    
-    std::vector<std::pair<int, int>> vecPair;
-    vecPair.emplace_back(1,2);
-    vecPair.push_back({5,6});
-    
-    
-    std::vector<int> v3(3, 100);
-    std::cout << v3[1] << " " << v3[0] << " "  << v3[2] << "\n";
+    // ===== Vector of Pairs =====
+    vector<pair<int, int>> vecPair;
+    vecPair.emplace_back(1, 2);
+    vecPair.push_back({5, 6});
 
-    std::vector<int> v2(v3);
-    std::cout << v2[1] << " " << v2[0] << " "  << v2[2] << "\n";
-    
-    std::vector<int> v5;
+    // ===== Pre-sized Vector =====
+    vector<int> v3(3, 100);   // {100, 100, 100}
+    vector<int> v2(v3);       // Copy constructor
+
+    // ===== Dynamic Insertions =====
+    vector<int> v5;
     v5.emplace_back(6);
     v5.emplace_back(7);
     v5.emplace_back(8);
 
+    // ===== Iterators =====
+    vector<int>::iterator it = v5.begin();
+    vector<int>::iterator it2 = v5.end();
 
-    
-    std::vector<int>::iterator it2 = v5.end();
-    std::cout << *(--it2) << "\n";
+    // cout << *(--it2) << "\n";     // last element
+    // cout << *it << " " << *(it + 2) << "\n";
 
-    
-    std::cout << v5.at(1) << "\n" << "this is v of back " << v5.back() << "\n";
-    std::vector<int>::iterator it = v5.begin();
-    std::cout << (*it) << "\n" << *(it+2) <<"\n";
-
-    
-    std::cout << "\n Using the for loop \n";
-     //printing the vector with the for loop
-    for(std::vector<int>::iterator it = v5.begin(); it != v5.end() ; it++){
-        std::cout << &it << " -> " << *(it) << "\n";
+    // ===== Iteration Using Iterators =====
+    for (vector<int>::iterator it = v5.begin(); it != v5.end(); it++) {
+        // cout << *it << " ";
     }
 
+    // ===== Auto Keyword =====
     auto s = "hello world";
-    std::cout << s << std::endl;
+    // cout << s << endl;
 
-    //this is a program 
-    
+    // ===== Final Test Vector =====
+    vector<int> final_test_vector = {
+        920, 1111, 1112, 222, 200, 101
+    };
 
+    // Range-based for loop
+    for (auto value : final_test_vector) {
+        cout << value << " ";
+    }
+    cout << "\n";
+
+    // ===== Erase =====
+    auto final_iterator = final_test_vector.begin();
+    final_test_vector.erase(final_iterator, final_iterator + 2);
+
+    // ===== Insert =====
+    final_test_vector.insert(final_test_vector.begin(), 920);
+    final_test_vector.insert(final_test_vector.begin() + 1, 2, 20);
+
+    vector<int> copy(2, 50);
+    final_test_vector.insert(
+        final_test_vector.begin(),
+        copy.begin(),
+        copy.end()
+    );
+
+    // ===== Pop Back =====
+    final_test_vector.pop_back();
+
+    // ===== Final Output =====
+    for (auto value : final_test_vector) {
+        cout << value << " ";
+    }
+    cout << "\n";
 }
